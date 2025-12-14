6ac77fa2957874a21e913e19a03f13985f737514
imjbassi <jaiveerbassi@yahoo.com>
2025-12-14 15:17:32 +0000

    update src/__init__.py

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+```python
+"""
+Neural Net Mapper - A package for mapping and visualizing neural network architectures.
+"""
+
+__version__ = "0.1.0"
+
+from .mapper import NeuralNetMapper
+from .visualizer import Visualizer
+
+__all__ = ["NeuralNetMapper", "Visualizer"]
+```
